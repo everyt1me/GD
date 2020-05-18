6f317a9c4f27613e17ca66eca27559d18359cfa3
Dexter <denus.denisovuch@gmail.com>
2020-05-18 18:16:59 +0300

    'header_v.0.0.7'

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,10 +1,2 @@
 import "%modules%/header/header";
-<<<<<<< HEAD
 
-
-=======
-import "%modules%/header/header_2";
-import "%modules%/footer/footer";
-// import "%modules%/banner/banner";
-// import "%modules%/main/main";
->>>>>>> 15d61cf9db76745ec32be60b57ecb536321e2b76
