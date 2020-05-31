b1da517a5f590adcb04d4f93c358369edda708ef
Dexter <denus.denisovuch@gmail.com>
2020-05-31 23:35:03 +0300

    Delete name page

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('guarantee.html', views.guarantee, name="guarantee"),
     path('apartment.html', views.apartment, name="apartment"),
     path('contact_us.html', views.contact_us, name="contact_us"),
-    path('testimonials.html', views.testimonials, name="testimonials1"),
+    path('testimonials.html', views.testimonials, name="testimonials"),
     path('discounts.html', views.discounts, name="discounts"),
 ]
 
